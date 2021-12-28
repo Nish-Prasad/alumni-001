b7548d406988a720f3dbdb43199b4ecbc7754e03
Nish-Prasad <80267281+Nish-Prasad@users.noreply.github.com>
2021-12-28 20:55:40 +0530

    login validation

--- script.js
+++ script.js
@@ -32,3 +32,62 @@ function toggleOther(){
         document.getElementById('extra-option').disabled = true;
     }
 }
+
+
+function login_validation() {
+    var fname = document.getElementById("fname").value;
+
+    if (fname == "") {
+      document.getElementById("fname1").innerHTML = "*field is empty";
+      return false;
+    } else if (fname.length > 20) {
+      document.getElementById("fname1").innerHTML =
+        "**enterd value is more than 20";
+      return false;
+    } else if (!isNaN(fname)) {
+      document.getElementById("fname1").innerHTML =
+        "***please enter only letters";
+      return false;
+    } else {
+      document.getElementById("fname1").innerHTML = "";
+    }
+
+
+    var text = document.getElementById("femail").value;
+    console.log(text);
+    var regx = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,3})$/;
+    if (regx.test(text) == false) {
+      document.getElementById("femail1").innerHTML = "invalid";
+      return false;
+    } else {
+      document.getElementById("femail1").innerHTML = "valid";
+    }
+
+    var fpassword = document.getElementById("fpassword").value;
+    if (fpassword == "") {
+        document.getElementById("fpassword1").innerHTML = "*field is empty";
+        return false;
+      } else if (fpassword.length > 20) {
+        document.getElementById("fpassword1").innerHTML =
+          "**enterd value is more than 20";
+        return false;
+      }else if (fpassword.length < 5) {
+        document.getElementById("fpassword1").innerHTML =
+          "**Password too short";
+        return false;
+      }else {
+        document.getElementById("fname1").innerHTML = "";
+      }
+  
+
+    var fphone = document.getElementById("fphone").value;
+    console.log(fphone);
+    var reg = /^[7-9]\d{9}$/;
+    if (reg.test(fphone) == false || fphone == "") {
+      document.getElementById("fphone1").innerHTML = "Invalid";
+      return false;
+    } else {
+      document.getElementById("fphone1").innerHTML = "valid";
+    }
+
+  }
